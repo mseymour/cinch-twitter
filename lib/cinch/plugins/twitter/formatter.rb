d93f23eb0830fcb40031c6c853d7b6f57f5e07db
Mark Seymour <mark.seymour.ns@gmail.com>
2012-06-28 12:31:40 -0300

    Including Cinch's Formatting module, renaming all instances of the "Format" method to "format"

--- lib/cinch/plugins/twitter/formatter.rb
+++ lib/cinch/plugins/twitter/formatter.rb
@@ -1,22 +1,24 @@
 # coding: utf-8
 require 'cgi'
+require 'cinch/formatting'
 
 module Cinch
   module Plugins
     class Twitter
       module Formatter
+        include Cinch::Formatting
 
         def format_tweet(tweet)
           tweet_text = expand_uris(tweet.text, tweet.attrs["entities"]["urls"])
           parts, head, body, tail, urls = [], [], [], [], []
-          head = Format(:bold,"#{tweet.user.screen_name} »")
+          head = format(:bold,"#{tweet.user.screen_name} »")
           body << CGI::unescapeHTML(tweet_text.gsub("\n", " ").squeeze(" "))
-          body << Format(:aqua,"*twoosh*") if tweet.text.length == 140
+          body << format(:aqua,"*twoosh*") if tweet.text.length == 140
           tail << "From #{tweet.place.full_name}" if !tweet.place.blank?
           tail << "at #{tweet.created_at.strftime("%B %-d, %Y, %-I:%m%P")}"
           tail << "via #{tweet.source.gsub( %r{</?[^>]+?>}, '' )}"
           urls << "https://twitter.com/#{tweet.user.screen_name}"
-          urls << Format(:grey,"in reply to") if !tweet.in_reply_to_screen_name.blank?
+          urls << format(:grey,"in reply to") if !tweet.in_reply_to_screen_name.blank?
           urls << "http://twitter.com/#{tweet.in_reply_to_screen_name}#{"/statuses/#{tweet.in_reply_to_status_id.to_s}" if !tweet.in_reply_to_status_id.blank?}" if !tweet.in_reply_to_screen_name.blank?
           parts = [head, body, ["(", tail.join(" "), ")"].join, urls].flatten
           parts.join(" ")
@@ -25,9 +27,9 @@ module Cinch
         def format_search(tweet)
           tweet_text = expand_uris(tweet.text, tweet.attrs["entities"]["urls"])
           parts, head, body, tail, urls = [], [], [], [], []
-          head = Format(:bold,"#{tweet.from_user} »")
+          head = format(:bold,"#{tweet.from_user} »")
           body << CGI::unescapeHTML(tweet_text.gsub("\n", " ").squeeze(" "))
-          body << Format(:aqua,"*twoosh*") if tweet.text.length == 140
+          body << format(:aqua,"*twoosh*") if tweet.text.length == 140
           tail << "at #{tweet.created_at.strftime("%B %-d, %Y, %-I:%m%P")}"
           urls << "https://twitter.com/#{tweet.from_user}"
           parts = [head, body, ["(", tail.join(" "), ")"].join, urls].flatten
@@ -36,11 +38,11 @@ module Cinch
 
         def format_tweep_info(tweep)
           tweep_status_text = expand_uris(tweep.status.text, tweep.status.attrs["entities"]["urls"])
-          head =  "#{Format(:aqua,tweep.name)}" + Format(:silver," (#{tweep.screen_name})") + Format(:grey," - #{tweep.url} https://twitter.com/#{tweep.screen_name}")
+          head =  "#{format(:aqua,tweep.name)}" + format(:silver," (#{tweep.screen_name})") + format(:grey," - #{tweep.url} https://twitter.com/#{tweep.screen_name}")
           bio = ""
-          bio = Format(:aqua,"\"#{tweep.description.strip}\"") if !tweep.description.blank?
+          bio = format(:aqua,"\"#{tweep.description.strip}\"") if !tweep.description.blank?
           location = ""
-          location = "They are from #{Format(:aqua,tweep.location.strip)}" if !tweep.location.blank?
+          location = "They are from #{format(:aqua,tweep.location.strip)}" if !tweep.location.blank?
           desc = [] << "has made #{tweep.statuses_count} tweets since #{tweep.created_at.strftime("%B %-d, %Y")}"
           desc << "follows #{tweep.friends_count} tweeps" if tweep.friends_count > 0
           desc << "has #{tweep.followers_count} followers" if tweep.followers_count > 0
@@ -51,14 +53,14 @@ module Cinch
           flags << "is a translator for Twitter" if tweep.is_translator?
           flags << "is verified" if tweep.verified?
           flags << "would rather keep their life secret" if tweep.protected?
-          tweet = [] << Format(:aqua,"Their latest tweet:")
+          tweet = [] << format(:aqua,"Their latest tweet:")
           tweet << CGI::unescapeHTML(tweep_status_text.gsub("\n", " ").squeeze(" "))
           tweet_tail = []
           tweet_tail << "from #{tweep.status.place.full_name}" if !tweep.status.place.blank?
           tweet_tail << "at #{tweep.status.created_at.strftime("%B %-d, %Y, %-I:%m%P")}"
 
           parts = [head, bio, location, desc, flags].reject(&:blank?).map {|e| e.is_a?(Array) ? "#{tweep.name} " + e.to_sentence + "." : e }
-          parts << [tweet, Format(:silver,["(", tweet_tail.join(" "), ")"].join)].join(" ")
+          parts << [tweet, format(:silver,["(", tweet_tail.join(" "), ")"].join)].join(" ")
           parts.join("\n")
         end
       
